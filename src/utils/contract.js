4d4d872fba0516126130b617dd042108df0099f2
Harsh Singh <harshsingh16d@gmail.com>
2025-05-24 10:47:23 +0530

    fix: updated path

--- src/utils/contract.js
+++ src/utils/contract.js
@@ -1,4 +1,4 @@
-import RichestRevealerABI from "@/abi/richestRevealer.json";
+import RichestRevealerABI from "../abi/RichestRevealer.json";
 
 export const RICHEST_REVEALER_CONTRACT_ADDRESS =
   "0x3B478B0baA26524df3D19d9528D0f3379Ade9129"; 
